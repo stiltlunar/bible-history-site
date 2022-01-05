e0e8947a72bd77aff0b9211bc715f688bd772ce4
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2022-01-05 11:19:19 -0600

    base testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "BibleHistory.info",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 };
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+function AboutPage() {
+  return (
+    <Layout pageTitle="About Page">
+      <p>This is my about page.</p>
+    </Layout>
+  );
+}
+
+export default AboutPage;
